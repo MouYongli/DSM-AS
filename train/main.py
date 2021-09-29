22b9a604c3f73b557260a3e833f654d0c37246d5
Yongli Mou <yongli.mou@rwth-aachen.de>
2021-09-29 23:41:10 +0200

    add performance eval and result save

--- train/main.py
+++ train/main.py
@@ -1,3 +1,4 @@
+import os
 import os.path as osp
 import numpy as np
 import pandas as pd
@@ -6,9 +7,28 @@ import torch
 import torch.nn as nn
 
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix
+
+## Get the env vars from station software
+# fhir_server = str(os.environ['FHIR_SERVER'])
+# fhir_port = str(os.environ['FHIR_PORT'])
+# mode = str(os.environ['MODE'])
+# station_name = str(os.environ['STATION_NAME'])
+
+# TODO: comment out when deploy
+station_name = "breugel"
+mode = "train"
 
 # Configurations
+seed = 7
+num_epochs = 10000
+input_dim = 30
+hidden_dim = 64
+num_classes = 2
+learning_rate = 0.01
+station_name = station_name.lower()
+mode = mode.lower()
+
 X_FEATURES = ['radius_mean', 'texture_mean', 'perimeter_mean',
        'area_mean', 'smoothness_mean', 'compactness_mean', 'concavity_mean',
        'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
@@ -20,19 +40,11 @@ X_FEATURES = ['radius_mean', 'texture_mean', 'perimeter_mean',
        'symmetry_worst', 'fractal_dimension_worst']
 Y_FEATURE = 'diagnosis'
 
-station_name = "breugel"
-mode = "train"
-
-seed = 7
-num_epochs = 10000
-input_dim = 30
-hidden_dim = 64
-num_classes = 2
-learning_rate = 0.01
-station_name = station_name.lower()
-mode = mode.lower()
-
-
+# Define directory of output
+here = osp.dirname(osp.abspath(__file__))
+out_dir = osp.join(here, 'output')
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
 
 class LogisticRegression(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_classes):
@@ -65,54 +77,109 @@ def valid(X_test, y_test, model, criterion):
     return val_loss.item(), (tp+tn)/(tp+fp+fn+tn), tp/(tp+fp), tp/(tp+fn), 2*tp/(2*tp+fn+fp)
 
 np.random.seed(seed)
-torch.manual_seed(seed)
-# Input csv df
+cuda = torch.cuda.is_available()
+torch.manual_seed(1337)
+if cuda:
+    torch.cuda.manual_seed(1337)
+
+"""
+@Laurenz, please replace here with your code for data extraction (FHIR -> DataFrame)
+"""
+###############################################################################################
 data = pd.read_csv('../input/data.csv')
 data = data.drop(['Unnamed: 32', 'id'], axis=1)
-data.diagnosis.replace(to_replace=dict(M=1, B=0), inplace=True)
+###############################################################################################
 
 
 
+"""
+@Toralf, please add the code for enlarging the dataset size.
+"""
+###############################################################################################
+
+###############################################################################################
+
+## Exploratory data analysis (EDA) and data normalization
 X_train = data[X_FEATURES]
 y_train = data[Y_FEATURE]
 scaler = StandardScaler()
-print("Mean: ", np.mean(X_train, axis=0).to_numpy())
-print("Std: ", np.std(X_train, axis=0).to_numpy())
+X_mean = np.mean(X_train, axis=0).to_numpy()
+X_std = np.std(X_train, axis=0).to_numpy()
+print("Mean: ", X_mean)
+print("Std: ", X_std)
 B, M = y_train.value_counts()
 print('Number of Benign: ', B)
 print('Number of Malignant : ', M)
 X_train = scaler.fit_transform(X_train)
+y_train.replace(to_replace=dict(M=1, B=0), inplace=True)
 y_train = y_train.to_numpy()
+## Saving statistical data for visualization
+stat_dir = os.path.join(out_dir, 'stat', station_name)
+if not os.path.exists(stat_dir):
+    os.makedirs(stat_dir)
+    df_X_mean = pd.DataFrame([X_mean], columns=X_FEATURES)
+    df_X_std = pd.DataFrame([X_std], columns=X_FEATURES)
+    df_y_dist = pd.DataFrame([[B, M]], columns=['B','M'])
+    df_X_mean.to_csv(osp.join(stat_dir, 'X_mean.csv'))
+    df_X_std.to_csv(osp.join(stat_dir, 'X_std.csv'))
+    df_y_dist.to_csv(osp.join(stat_dir, 'X_std.csv'))
+
 
 model = LogisticRegression(input_dim, hidden_dim, num_classes)
-if osp.exists("./model.pth.tar"):
-    checkpoint = torch.load("./model.pth.tar")
-    model.load_state_dict(checkpoint['model_state_dict'])
-
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
-# loss_list = []
-# val_loss_list = []
-# val_acc_list = []
-# val_precision_list = []
-# val_recall_list = []
-# val_f1_score_list = []
+model_dir = os.path.join(out_dir, 'models')
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+    torch.save({
+        'epoch': -1,
+        'optim_state_dict': optimizer.state_dict(),
+        'model_state_dict': model.state_dict(),
+        'best_acc': 0.0,
+    }, osp.join(model_dir, 'dnn.pth.tar'))
+else:
+    checkpoint = torch.load(osp.join(model_dir, "dnn.pth.tar"))
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optim_state_dict'])
+
+loss_list = []
+val_loss_list = []
+val_acc_list = []
+val_precision_list = []
+val_recall_list = []
+val_f1_score_list = []
 for epoch in range(num_epochs):
     perm = np.arange(X_train.shape[0])
     np.random.shuffle(perm)
     X_train = X_train[perm]
     y_train = y_train[perm]
     loss = train(X_train, y_train, model, criterion, optimizer)
-    val_loss, accuracy, precesion, recall, f1_score = valid(X_train, y_train, model, criterion)
+    val_loss, val_acc, val_precesion, val_recall, val_f1_score = valid(X_train, y_train, model, criterion)
+    loss_list.append(loss)
+    val_loss_list.append(val_loss)
+    val_acc_list.append(val_acc)
+    val_precision_list.append(val_precesion)
+    val_recall_list.append(val_recall)
+    val_f1_score_list.append(val_f1_score)
     if epoch % 1000 == 0:
         print('==================================================================')
         print('Epoch', epoch)
-        # print('Train loss: ', loss)
-        # print('Val loss: ', val_loss)
-        print('Val acc : ', accuracy)
-        # print('Val precesion : ', precesion)
-        # print('Val recall : ', recall)
-        # print('Val f1_score : ', f1_score)
+        print('Train loss: ', loss)
+        print('Val loss: ', val_loss)
+        print('Val acc : ', val_acc)
+        print('Val precesion : ', val_precesion)
+        print('Val recall : ', val_recall)
+        print('Val f1_score : ', val_f1_score)
+
+# plot learning curve
+plt.figure()
+plt.plot(range(num_epochs), loss_list, 'r-', label='train_loss')
+plt.plot(range(num_epochs), val_loss_list, 'b-', label='val_loss')
+plt.plot(range(num_epochs), val_acc_list, 'g-', label='val_acc')
+plt.plot(range(num_epochs), val_recall_list, 'r+', label='val_recall')
+plt.plot(range(num_epochs), val_f1_score_list, 'g+', label='val_f1_score')
+plt.legend()
+plt.show()
 
 
